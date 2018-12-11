c29455968c778b35baa6d096f68e3ebd29c74bdf
TYuya <grgms35@gmail.com>
2018-12-11 17:57:31 +0900

    project directry

---
+++ KUMA/app/src/main/java/com/seven_dream/kuma/TitleController.kt
@@ -0,0 +1,12 @@
+package com.seven_dream.kuma
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class TitleController : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.titlecontroller)
+    }
+}
