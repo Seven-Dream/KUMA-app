811798e766bdde8979ade789e81122ef1c738894
YokotaAya380 <200380m@ugs.kochi-tech.ac.jp>
2019-01-12 00:37:43 +0900

    TimetableResult.kt:ボタンでの動作を追加(未完)

--- KUMA/app/src/main/java/com/seven_dream/kuma/TimetableResult.kt
+++ KUMA/app/src/main/java/com/seven_dream/kuma/TimetableResult.kt
@@ -2,11 +2,27 @@ package com.seven_dream.kuma
 /* 講義検索結果画面の動作 */
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_timetable_result.*
 
 class TimetableResult : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_timetable_result)
+
+        /* 講義登録ボタン */
+        //講義登録ボタンを押した時の動作
+        /*
+        regist_button.setOnClickListener {
+
+        }
+         */
+
+        /* 戻るボタン */
+        //戻るボタンを押したら検索結果画面の表示を終了（講義検索画面へ遷移）
+        return_result.setOnClickListener {
+            finish()
+        }
+
     }
 }
