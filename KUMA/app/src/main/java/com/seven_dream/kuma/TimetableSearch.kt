11a8f4cb593c6a2bcc7863420041c43e1fb3bde0
YokotaAya380 <200380m@ugs.kochi-tech.ac.jp>
2019-01-16 06:29:17 +0900

    TimetableSearch.ktで検索処理を記述（未完）

--- KUMA/app/src/main/java/com/seven_dream/kuma/TimetableSearch.kt
+++ KUMA/app/src/main/java/com/seven_dream/kuma/TimetableSearch.kt
@@ -1,5 +1,6 @@
 package com.seven_dream.kuma
 /* 講義検索画面での動作 */
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.layout_timetable_search.*
@@ -7,67 +8,34 @@ import android.widget.AdapterView
 import android.widget.Spinner
 import android.widget.ArrayAdapter
 import android.view.View
+import com.example.androiddev.myapplication.userDB_Helper
+
+//import com.example.androiddev.myapplication.userDB_Helper
 
 
 class TimetableSearch :  AppCompatActivity() {
+    // userDB_Adapter_Timetableクラスを定義
+    private lateinit var userDB: DB_Adapter_Search_Timetable
 
-    //対象年次プルダウンの選択肢
-    private val spinnerGrades = arrayOf("--", "1年次・回生", "2年次・回生", "3年次・回生", "4年次・回生", "その他")
     //開講クウォータプルダウンの選択肢
     private val spinnerQuarters = arrayOf("--", "1Q", "2Q", "3Q","4Q")
-    //対象学群プルダウンの選択肢
-    private val spinnerTargets = arrayOf("--", "システム工学群", "環境理工学群", "情報学群", "経済・マネジメント学群", "共通科目")
+
     //override
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout_timetable_search)
+        //DBの呼び出し
+        userDB = DB_Adapter_Search_Timetable(this)
 
         /* プルダウン機能 */
-        /* 対象学年 */
-        //ArrayAdapter
-        val adapterGrade = ArrayAdapter(applicationContext, android.R.layout.simple_spinner_item, spinnerGrades)
-        adapterGrade.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-
-        //spinnerにadapterをセット
-        //Kotlin Android Extensions
-        spinner_grade.adapter = adapterGrade
-
-        //リスナーを登録
-        spinner_grade.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
-            //アイテムが選択されたとき
-            override fun onItemSelected(parent: AdapterView<*>?,
-                                        view: View?, position: Int, id:Long) {
-                val spinnerParent = parent as Spinner
-                val gradeItem = spinnerParent.selectedItem as String
-                if(gradeItem != "--") {
-                    val selectGrade: String = gradeItem
-                    //DBと比較（対象年次）
-
-                    //DBと一致する講義のIDを取得：配列に格納
-                    val gradeLecture:Array<Int> = arrayOf() //arrayOfの中にIDをいれる
-                } else {
-                    //「--」が選択されたままのとき、nullとして扱う
-                    val gradeLecture: String? = null
-                    //
-                }
-            }
-            /*
-            //アイテムが選択されなかったとき
-            override fun onNothingSelected(parent: AdapterView<*>?){
-                //
-            }
-            */
-        }
-
         /* 開講クウォータ */
         //ArrayAdapter
         val adapterQuarter = ArrayAdapter(applicationContext, android.R.layout.simple_spinner_item, spinnerQuarters)
         adapterQuarter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-
         //spinnerにadapterをセット
         //Kotlin Android Extensions
         spinner_quarter.adapter = adapterQuarter
-
+        var quarterData: Int? = null
         //リスナーを登録
         spinner_quarter.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
             //アイテムが選択されたとき
@@ -77,44 +45,24 @@ class TimetableSearch :  AppCompatActivity() {
                 val quarterItem = spinnerParent.selectedItem as String
                 if (quarterItem != "--") {
                     val selectQuarter: String = quarterItem
-                    //DBと比較（開講クウォータ）
-
-                    //DBと一致する講義のIDを取得：配列に格納
-                    val quarterLecture: Array<Int> = arrayOf() //arrayOfの中にIDをいれる
+                    if(selectQuarter == "1Q") {
+                        quarterData = 1
+                    } else if(selectQuarter == "2Q") {
+                        quarterData = 2
+                    } else if(selectQuarter == "3Q") {
+                        quarterData = 3
+                    } else if(selectQuarter == "4Q") {
+                        quarterData = 4
+                    } else if(selectQuarter == "1学期") {
+                        quarterData = 5
+                    } else if(selectQuarter == "2学期") {
+                        quarterData = 6
+                    } else if(selectQuarter == "通年") {
+                        quarterData = 7
+                    }
                 } else {
                     //「--」が選択されたままのとき、nullとして扱う
-                    val quarterLecture: String? = null
-                    //
-                }
-            }
-        }
-
-        /* 対象学群 */
-        //ArrayAdapter
-        val adapterTarget = ArrayAdapter(applicationContext, android.R.layout.simple_spinner_item, spinnerTargets)
-        adapterTarget.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-
-        //spinnerにadapterをセット
-        //Kotlin Android Extensions
-        spinner_target.adapter = adapterTarget
-
-        //リスナーを登録
-        spinner_target.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
-            //アイテムが選択されたとき
-            override fun onItemSelected(parent: AdapterView<*>?,
-                                        view: View?, position: Int, id:Long) {
-                val spinnerParent = parent as Spinner
-                val targetItem = spinnerParent.selectedItem as String
-                if (targetItem != "--") {
-                    val selectTarget: String = targetItem
-                    //DBと比較（対象学群）
-
-                    //DBと一致する講義のIDを取得：配列に格納
-                    val targetLecture: Array<Int> = arrayOf() //arrayOfの中にIDをいれる
-                } else {
-                    //「--」が選択されたままのとき、nullとして扱う
-                    val targetLecture: String? = null
-                    //
+                    quarterData = null
                 }
             }
         }
@@ -122,30 +70,107 @@ class TimetableSearch :  AppCompatActivity() {
         /* 検索機能 */
         //文字入力が終了した状態にて、検索ボタンを使って文字列を取り出す
         search_button.setOnClickListener {
-            /* format1(講義名)の処理 */
-            if(format1.text != NULL) {
-                //format1に入力がある場合、DBと比較
+            //DBを取得
+            //val db = userDB.writableDatabase //val db = userDB!!.writableDatabase
 
-                //DBと一致する講義のIDを取得：配列に格納(入力された文字列が講義名に含まれるものの講義IDを格納)
-                //format1の結果でえられたIDを格納する配列の宣言
-                val nameLecture:Array<Int> = arrayOf() //arrayOfの中にIDをいれる
-            } else {
-                //NULLの場合、配列にはデータベース上の講義IDをすべて格納する
-                val nameLecture:Array<Int> = arrayOf() //arrayOfの中にIDをいれる
+            //検索結果(lecture_id)を格納する
+            var resultLecture: Array<Int> = arrayOf()
+            var resultTeacher: Array<Int> = arrayOf()
+            var resultQuarter: Array<Int> = arrayOf()
+            var resultPrint: Array<Int> = arrayOf()//最終的な検索結果
+
+
+                /* format1(講義名)の処理 */
+                if (format1.text != null) {
+                    val i: Int = userDB.countLecture(format1.text.toString())
+                    var insertTemp = 0 //結果を入れる配列の場所
+                    //講義検索の結果を配列に入れる
+                    for (cou in 0..i) {
+                        //format1に入力がある場合、講義名にformat1の文字列を含む講義の講義IDをDBから取得
+                        val nameLecture: Int = userDB.getLecture_idByName(format1.text, cou)
+                        //format1の結果でえられたIDを格納する配列の宣言
+                        //val resultLecture = arrayOf(nameLecture) //arrayOfの中にIDをいれる
+                        resultLecture[insertTemp] = nameLecture //arrayOfの中にIDをいれる
+                        insertTemp += 1
+                    }
+                } else {
+                    //NULLの場合、配列にはデータベース上の講義IDをすべて格納する
+                    val nameLecture: Int = userDB.getAllLecture()
+                    //val resultLecture: Array<Int> = arrayOf(nameLecture) //arrayOfの中にIDをいれる
+                    resultLecture = arrayOf(nameLecture) //arrayOfの中にIDをいれる
+                }
+
+                /* format2(教員名)の処理 */
+                if (format2.text != null) {
+                    val i: Int = userDB.countLecture(format2.text.toString())
+                    var insertTemp = 0 //結果を入れる配列の場所
+                    for (cou in 0..i) {
+                        //format1に入力がある場合、講義名にformat1の文字列を含む講義の講義IDをDBから取得
+                        val teacherLecture: Int = userDB.getLecture_idByTeach(format2.text, cou)
+                        //format1の結果でえられたIDを格納する配列の宣言
+                        //val resultTeacher: Array<Int> = arrayOf(teacherLecture) //arrayOfの中にIDをいれる
+                        resultTeacher[insertTemp] = teacherLecture //arrayOfの中にIDをいれる
+                        insertTemp += 1
+                    }
+                } else {
+                    //NULLの場合、配列にはデータベース上の講義IDをすべて格納する
+                    val teacherLecture: Int = userDB.getAllLectureByTeach()
+                    //val resultTeacher: Array<Int> = arrayOf(nameLecture) //arrayOfの中にIDをいれる
+                    resultTeacher = arrayOf(teacherLecture) //arrayOfの中にIDをいれる
+                }
+
+                /* クウォータ */
+                // quarterLectureと講義(lecture)テーブルの開講時期(quarter)が一致するとき
+                if (quarterData != null) {
+                    val i: Int = userDB.countLecture()
+                    for (cou in 0..i) {
+                        //format1に入力がある場合、講義名にformat1の文字列を含む講義の講義IDをDBから取得
+                        val quarterLecture: Int = userDB.getLecture_idByQuarter(quarterData, cou)
+                            userDB.getLecture_idByQuarter(quarterLecture, cou)//i番目のlecture_idをとってくる
+                        //format1の結果でえられたIDを格納する配列の宣言
+                        //val resultQuarter: Array<Int> = arrayOf(quarterLecture) //arrayOfの中にIDをいれる
+                        resultQuarter = arrayOf(quarterLecture) //arrayOfの中にIDをいれる
+                    }
+                } else {
+                    //NULLの場合、配列にはデータベース上の講義IDをすべて格納する
+                    val quarterLecture: Int = userDB.getAllLectureByQuarter()
+                    //val resultQuarter: Array<Int> = arrayOf(quarterLecture) //arrayOfの中にIDをいれる
+                    resultQuarter = arrayOf(quarterLecture) //arrayOfの中にIDをいれる
+                }
+
+                /* ①：講義名から取得したID・教員名から取得したIDを比較 */
+                /* ②：①で取得したID・開講クウォータから取得したIDを比較 */
+                val i: Int = userDB.getMaxLecture()//講義テーブルにあるlecture_idの最大値を取得
+                var insertTmp = 0
+                for (id in 0..i) {//講義テーブルに入ってるid全部で比較する
+                    //resultLecture内にlectureテーブル[lecture_id]のi番目の講義IDが含まれるかどうか
+                    val checkLecture = resultLecture.filter { it == id }
+                    //resultTeacher内にlectureテーブル[lecture_id]のi番目の講義IDが含まれるかどうか
+                    val checkTeacher = resultTeacher.filter { it == id }
+                    //resultQuarter内にlectureテーブル[lecture_id]のi番目の講義IDが含まれるかどうか
+                    val checkQuarter = resultQuarter.filter { it == id }
+
+                    if (checkLecture[0] == id) {
+                        if (checkTeacher[0] == id) {
+                            if (checkQuarter[0] == id) {
+                                //最終的な結果を入れる配列(resultPrint)にidを格納する
+                                resultPrint[insertTmp] = id
+                                insertTmp += 1
+                            }
+                        }
+                    }
+                }
             }
 
-            /* ①：講義名から取得したID・対象年次から取得したIDを比較 */
-            /* ②：①で取得したID・開講クウォータから取得したIDを比較 */
-            /* ③：②で取得したID・対象学群から取得したIDを比較 */
-        }
+
+
 
         /* 戻るボタン */
-        /*
         return_search.setOnClickListener {
-            val intent = Intent(application, Timetable::class.java)
+            val intent = Intent(application, TitleController::class.java) // 本当は違うよ、テストだよ
+            // val intent = Intent(application, Timetable::class.java)
             startActivity(intent)
         }
-        */
     }
 
 }
