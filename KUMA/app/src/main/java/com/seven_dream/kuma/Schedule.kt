c0c1fe086e522b4f15f32626671b7ac8cc8d7702
Shiota07 <45251016+Shiota07@users.noreply.github.com>
2019-01-21 02:29:45 +0900

    時間割に画面遷移できるようにした

--- KUMA/app/src/main/java/com/seven_dream/kuma/Schedule.kt
+++ KUMA/app/src/main/java/com/seven_dream/kuma/Schedule.kt
@@ -3,27 +3,34 @@ package com.seven_dream.kuma
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import kotlinx.android.synthetic.main.activity_schedule.*
 
 class Schedule : AppCompatActivity() {
+
     internal lateinit var db:DBHelper
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_schedule)
 
         db = DBHelper(this)
-        calendarView.setOnDateChangeListener { view, year, month, dayOfMonth ->
 
+        calendarView.setOnDateChangeListener { view, year, month, dayOfMonth ->
             // 日付を取得し、DBHelper.ktで使えるようにDBHelperに格納する
             val date = "" + year + "/" + (month + 1) + "/" + dayOfMonth
             db.getDate(date)
 
             val intent: Intent = Intent(this, ScheduleOneday::class.java)
             intent.putExtra("date", date)
-            //intent.putExtra("year", year)
-            //intent.putExtra("month", month)
-            //intent.putExtra("dayOfMonth", dayOfMonth)
             startActivity(intent)
         }
+
+        // 時間割画面に飛ぶ
+        /*val timeSchedule = findViewById<Button>(R.id.timeSchedule)
+        timeSchedule.setOnClickListener {
+            val intent1 = Intent(this,ScheduleOneday::class.java)
+            startActivity(intent1)
+        }*/
     }
 }
